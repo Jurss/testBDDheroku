7f696631947240408d0d69529ff902eda04ab71d
Jurss <remi62120@hotmail.fr>
2021-09-18 11:51:32 +0200

    test

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <?php
 
-
-$url = getenv('JAWSDB_MARIA_URL');
-$dbparts = parse_url($url);
-
-$hostname = $dbparts['f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com'];
-$username = $dbparts['x8hl57bywc8di1sm'];
-$password = $dbparts['pzr739bpfpiia8eq'];
-$database = ltrim($dbparts['jd75vt0lw59dx2u6'], '/');
-
-
 try {
-    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    $conn = new PDO("mysql:host=185.98.131.109;dbname=remid1641152", 'remid1641152', 'pjsvof51do');
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
